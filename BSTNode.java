6fde982ba995ba6541f169ed1756fe9dc0eeabb0
Jupichoe <56129840+Jupichoe@users.noreply.github.com>
2021-06-03 22:42:05 -0500

    Update BSTNode.java

--- BSTNode.java
+++ BSTNode.java
@@ -1,241 +1,47 @@
 //Daniel Choe
-//CS 304 Data Structures
 
-
-package chapter07;
-
-public class BinarySearchTree
+public class BSTNode
 {
-   protected BSTNode root;
-   protected boolean success;
-
-   public BinarySearchTree()
+   private int info;
+   private BSTNode left;
+   private BSTNode right;
+   
+   public BSTNode(int info)
    {
-      this.root = null;
+      this.info = info;
+      this.left = null;
+      this.right = null;
    }
    
-   public boolean contains(int value)
+   public void setInfo(int info)
    {
-      BSTNode curr = root;
-
-      while(curr != null)
-      {
-         if(value == curr.getInfo())
-         {
-            return true;
-         }
-         else if(value < curr.getInfo())
-         {
-            curr = curr.getLeft();
-         }
-         else
-         {
-            curr = curr.getRight();
-         }
-      }
-      return false;
-   }
-
-   public boolean add(int value)
-   {
-      root = add(value, root);
-      return success;
-   }
-
-   private BSTNode add(int value, BSTNode curr)
-   {
-      if(curr == null)
-      {
-         curr = new BSTNode(value);
-         success = true;
-      }
-      else if(value < curr.getInfo())
-      {
-         curr.setLeft(add(value, curr.getLeft()));
-      }
-      else if(value > curr.getInfo())
-      {
-         curr.setRight(add(value, curr.getRight()));
-      }
-      else
-      {
-         success = false;
-      }
-      return curr;
+      this.info = info;
    }
    
-   public void preOrder()
+   public int getInfo()
    {
-      System.out.print("Pre Order: ");
-      preOrder(root);
-      System.out.println();
-   }
-   private void preOrder(BSTNode curr)
-   {
-      if(curr != null)
-      {
-         System.out.print(curr.getInfo() + " ");
-         preOrder(curr.getLeft());
-         preOrder(curr.getRight());
-      }
-   }
-
-   public void inOrder()
-   {
-      System.out.print("In Order: ");
-      inOrder(root);
-      System.out.println();
-   }
-   private void inOrder(BSTNode curr)
-   {
-      if(curr != null)
-      {
-         inOrder(curr.getLeft());
-         System.out.print(curr.getInfo() + " ");
-         inOrder(curr.getRight());
-      }
-   }
-
-   public void postOrder()
-   {
-      System.out.print("Post Order: ");
-      postOrder(root);
-      System.out.println();
-   }
-   private void postOrder(BSTNode curr)
-   {
-      if(curr != null)
-      {
-         postOrder(curr.getLeft());
-         postOrder(curr.getRight());
-         System.out.print(curr.getInfo() + " ");
-      }
-   }
-
-   public int countLeaves()
-   {
-      
-      return countLeaves(root);
-   }
-   public int countLeaves(BSTNode curr)
-   {
-      if(curr == null)
-      {
-         return 0;
-      }
-      if(curr.getLeft() == null && curr.getRight() == null)
-      {
-         return 1;
-      }
-      else
-      {
-         return countLeaves(curr.getLeft()) + countLeaves(curr.getRight());
-      }
-   }
-
-
-   public int size()
-   {
-      return size(root);
+      return this.info;
    }
    
-   public int size(BSTNode curr)
+   public void setRight(BSTNode right)
    {
-      if(curr == null)
-      {
-         return 0;
-      }
-      else
-      {
-         return size(curr.getLeft()) + size(curr.getRight()) + 1;
-      }
-   }
-
-
-   public int height()
-   {
-      return height(root);
+      this.right = right;
    }
    
-   public int height(BSTNode curr)
+   public BSTNode getRight()
    {
-      if(curr == null)
-      {
-         return 0;
-      }
-      else
-      {
-         int leftHeight = 0, rightHeight = 0;
-         
-         if(curr.getLeft() != null)
-         {
-            leftHeight = height(curr.getLeft());
-         }
-         if(curr.getRight() != null)
-         {
-            rightHeight = height(curr.getRight());
-         }
-         
-         return 1 + Math.max(leftHeight, rightHeight);
-      }
+      return this.right;
    }
-      
-   public boolean remove(int target)
-   {
-      root = remove(target, root);
-      return success;
-   }
-
-   private BSTNode remove(int target, BSTNode curr)
-   {
-      if(curr == null)
-      {
-         success = false;
-      }
-      else if(target < curr.getInfo())
-      {
-         curr.setLeft(remove(target, curr.getLeft()));
-      }
-      else if(target > curr.getInfo())
-      {
-         curr.setRight(remove(target, curr.getRight()));
-      }
-      else
-      {
-         curr = removeNode(curr);
-         success = true;
-      }
-      return curr;
-   }
-
-   private BSTNode removeNode(BSTNode curr)
-   {
-
-      if(curr.getLeft() == null)
-      {
-         return curr.getRight();
-      }
-      else if(curr.getRight() == null)
-      {
-         return curr.getLeft();
-      }
-      else
-      {
-         int data = getRightMostData(curr.getLeft()); 
-         curr.setInfo(data); 
-         curr.setLeft(remove(data, curr.getLeft())); 
-         return curr;
-      }
-   }
-
-   private int getRightMostData(BSTNode subtree) 
-   {
-      BSTNode temp = subtree; 
    
-      while (temp.getRight() != null) 
-      {
-         temp = temp.getRight(); 
-      }
-      return temp.getInfo(); 
+   public void setLeft(BSTNode left)
+   {
+      this.left = left;
+   }
+   
+   public BSTNode getLeft()
+   {
+      return this.left;
    }
 }
+
+
